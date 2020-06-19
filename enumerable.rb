36ebb2860c316d13eff2267d5d6bc3e4695c26e7
ldelbel <lucdelbel@gmail.com>
2020-06-19 10:31:55 -0300

    Create my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -10,3 +10,14 @@
   # end
 
   my_each([1, 2, 3, 4, 5]) {|x| puts x * 2}
+
+  def my_each_with_index(arr)
+    i = 0
+    results = []
+      while i < arr.length
+        results << yield(arr[i],i)
+        i += 1
+      end
+  end
+
+  my_each_with_index([1, 2, 3, 4, 5]) {|val, index| puts "index: #{index} for val: #{val}"}
