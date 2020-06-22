85b1626cc24ecea7e110c94ca27798bee1276324
Georjane <60772732+Georjane@users.noreply.github.com>
2020-06-22 16:58:45 +0100

    Correct my_each, my_each_with_index, upto my_count

--- script.rb
+++ script.rb
@@ -1,85 +1,58 @@
 module Enumerable
-  def my_each(arr)
+  def my_each
+    return to_enum unless block_given?
     i = 0
-    while i < arr.length
-      yield(arr[i])
+    while i < length
+      yield(self[i])
       i += 1
     end
-    return arr
-  end
-  
-  def my_each_with_index(arr)
-    i = 0
-    results = []
-    while i < arr.length
-      results << yield(arr[i],i)
-      i += 1
-    end
+    return self
   end
 
-  def my_select(arr)
-    i = 0
+  def my_each_with_index
+    return to_enum unless block_given?
     results = []
-      while i < arr.length
-        if yield(arr[i])
-          results << yield(arr[i])
-        end
-        i += 1
-      end
+    my_each {|x| results << yield(x, index(x))}
   end
 
-  def my_all?(arr)
-    i = 0
+  def my_select
+    return to_enum unless block_given?
+    results = []
+    my_each {|x| results << x if yield(x)}    
+    return results
+  end
+
+  def my_all?
     check = true
-      while i < arr.length
-        if !yield(arr[i])
-          check = false
-        end
-        i += 1
-      end
-      return check
+    my_each {|x| check = false if !yield(x)}
+    return check 
   end
 
-  def my_any?(arr)
-    i = 0
+  def my_any?
     check = false
-      while i < arr.length
-        if yield(arr[i])
-          check = true
-        end
-        i += 1
-      end
-      return check
+    my_each {|x| check = true if yield(x)}
+    return check  
   end
 
-  def my_none?(arr)
-    i = 0
+  def my_none? 
     check = true
-      while i < arr.length
-        if yield(arr[i])
-          check = false
-        end
-        i += 1
+    if block_given?      
+      my_each {|x| check = false if yield(x)}     
+    else       
+      unless self.nil? || self.empty? 
+        check = false
       end
-      return check
-  end
-
-  def my_count(arr)
-    i = 0
-    j = 0
-    if block_given?
-      while i < arr.length
-        if yield(arr[i])
-          j += 1
-        end
-        i += 1
-      end
-      return j
-    else 
-      return arr.length
     end
+    return check    
   end
 
+  def my_count
+    return self.size unless block_given?
+    j = 0
+    my_each {|x| j += 1 if yield(x)} 
+    return j    
+  end
+# Correct methods below
   def my_map(arr)
     i = 0
     results = []
@@ -88,4 +61,46 @@ module Enumerable
       i += 1
     end
   end
+
+ def my_inject(*args)
+  array = [9, 20, 11]
+  symbol = args[1]
+  result = args[0]
+  
+  if block_given? 
+    i = 0
+    if args[0].nil?
+      result = array[0]
+      while i < array.length - 1
+      result = yield(result, array[i+1])
+      i += 1
+      end
+    elsif args[0].integer? && args[1].nil?
+      while i < array.length
+      result = yield(result, array[i])
+      i += 1
+      end
+    end
+  else
+    if args[1].nil?
+      symbol = args[0]
+      case symbol 
+       when :+ || :-
+        res = 0
+       else 
+        res = 1
+       end      
+      array.each { |ar| res = res.send(symbol, ar)}
+      return res
+    else
+      array.each { |ar| result = result.send(symbol, ar)}
+    end
+  end
+  return result
+ end
+
+ def multiply_els(arr)   
+ end
+
+ 
 end
